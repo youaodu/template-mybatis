5a2696d478f9a68abbc35109064075f4d001b178
youao.du <youao.du@gmail.com>
2020-10-15 10:12:35 +0800

    添加  数据权限增加

---
+++ template-admin/template-admin-application/src/main/java/com/youaodu/template/admin/application/config/DataAuthPlugin.java
@@ -0,0 +1,48 @@
+package com.youaodu.template.admin.application.config;
+
+import org.apache.ibatis.executor.statement.StatementHandler;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.plugin.*;
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.util.Properties;
+
+@Component
+@Intercepts({
+        @Signature(
+                type = StatementHandler.class, method = "prepare", args = {Connection.class, Integer.class
+        })
+})
+public class DataAuthPlugin implements Interceptor {
+
+    /**
+     *
+     * @param invocation
+     * @return
+     */
+    @Override
+    public Object intercept(Invocation invocation) throws Throwable {
+        StatementHandler statementHandler = (StatementHandler) invocation.getTarget();
+        BoundSql boundSql = statementHandler.getBoundSql();
+        String sql = boundSql.getSql();
+
+        // TODO 数据权限订正加上个人标记
+
+        return invocation.proceed();
+    }
+
+    @Override
+    public Object plugin(Object target) {
+        if (target instanceof StatementHandler) {
+            return Plugin.wrap(target, this);
+        } else {
+            return target;
+        }
+    }
+
+    @Override
+    public void setProperties(Properties properties) {
+
+    }
+}
